b74aae81eda356151abd97a064f99cc8d008167c
James Zhu <zhujian0805@gmail.com>
2015-09-26 21:25:36 +0800

    mod

---
+++ express-01.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+   console.log("received hello world")
+   res.send('Hello World');
+})
+
+var server = app.listen(8081, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://%s:%s", host, port)
+
+})
